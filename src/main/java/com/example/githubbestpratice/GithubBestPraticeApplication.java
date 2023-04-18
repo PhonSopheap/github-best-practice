def7a2c13c1be2d1163b54b97320bec3c24aaeb3
S0pheap <sopheapphon2001@gmail.com>
2023-04-18 12:02:46 +0700

    Initial spring-boot

---
+++ src/main/java/com/example/githubbestpratice/GithubBestPraticeApplication.java
@@ -0,0 +1,13 @@
+package com.example.githubbestpratice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubBestPraticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubBestPraticeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/githubbestpratice/GithubBestPraticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.githubbestpratice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubBestPraticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
